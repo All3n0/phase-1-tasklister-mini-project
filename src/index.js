b3eeafd11be566349258a8477793ba4c4bfc0c3e
ALLAN <allankipr6@gmail.com>
2024-03-29 12:24:44 +0300

    new

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskForm=document.querySelector('form')
+  taskForm.addEventListener('submit',(callback)=>{
+    callback.preventDefault();
+    const newTodo=callback.target.querySelector('#new-task-description').value;
+    buildToDo(newTodo);
+    taskForm.reset();//to make the input empty again.
+  })
 });
+function buildToDo(newTodo){
+  const makeList=document.createElement('li');
+  const removebutton=document.createElement('button')
+
+  removebutton.textContent= 'remove'
+  makeList.textContent=newTodo;
+  makeList.appendChild(removebutton);
+  document.querySelector('#tasks').appendChild(makeList)
+  removebutton.addEventListener('click',deletee);
+}
+function deletee(callback){
+  callback.target.parentNode.remove();
+}
